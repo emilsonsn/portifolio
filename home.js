5ac1fcbe2f7af09e91ec5685ec0695bbd7949462
emilsonsd <emilsonsn2@gmail.com>
2021-09-30 14:01:46 -0300

    criando o home do portifolio

---
+++ home.js
@@ -0,0 +1,27 @@
+incrementador = 0;
+    function aumentar(){
+    if(document.getElementById("img-1").style.opacity <=0){
+        const set = setInterval(() => {
+            document.getElementById("img-1").style.opacity = incrementador
+            incrementador=incrementador+0.1
+            if(document.getElementById("img-1").style.opacity>=1){
+                clearInterval(set);
+                diminuir()
+            }
+        }, 50);
+    }
+}
+    function diminuir(){
+        if(document.getElementById("img-1").style.opacity >=1){
+            const set = setInterval(() => {
+                document.getElementById("img-1").style.opacity = incrementador
+                incrementador=incrementador-0.1
+                if(document.getElementById("img-1").style.opacity<=0){
+                    clearInterval(set);
+                    aumentar()
+            }
+        }, 50);
+    }
+}
+
+aumentar()
